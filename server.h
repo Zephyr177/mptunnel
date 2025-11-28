f690b493ab3eba78ee1ab8a18a455d5c2f6b4000
Zephyr177 <email@example.com>
2025-11-28 14:37:40 +0800

    build failed

--- server.h
+++ server.h
@@ -1,18 +1,11 @@
 #ifndef __SERVER_H__
 #define __SERVER_H__
 
+#include "platform.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 
 #include "linklist.h"
 
